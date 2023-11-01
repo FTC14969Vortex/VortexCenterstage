1a10c2ada1dcd90997aad5c03b5a0d4d56c41c52
Shashvat Mehta <68447474+smehtacoder@users.noreply.github.com>
2023-11-01 00:04:45 -0700

    Changed tensorflow test to auto instead of teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TensorflowTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TensorflowTest.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode.OpModes;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -48,7 +49,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "TensorFlow Object Detection Test", group = "Concept")
+@Autonomous(name = "TensorFlow Object Detection Test", group = "Concept")
 public class TensorflowTest extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
