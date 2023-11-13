9a90c95953013f5c081dc793e377194b75f343eb
Shashvat Mehta <68447474+smehtacoder@users.noreply.github.com>
2023-11-12 16:50:45 -0800

    April Tags working with old robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Chassis.java
@@ -13,11 +13,40 @@ import java.util.*;
 
 public class Chassis {
 
+    // global location.
+    public int robotX = 0;
+    public int robotY = 0;
+
+    public int[] Location = {robotX,robotY};
+
+    public  BNO055IMU imu;
+
+    public Orientation angles;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    int timeout_ms;
+
     //Drivetrain Motor
     public DcMotor FLMotor = null;
     public DcMotor FRMotor = null;
     public DcMotor BLMotor = null;
     public DcMotor BRMotor = null;
+
+
+    //IMU
+    //How many times the encoder counts a tick per revolution of the motor.
+    static final double COUNTS_PER_MOTOR_REV_Hex= 538; // https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-19-2-1-ratio-24mm-length-8mm-rex-shaft-312-rpm-3-3-5v-encoder/
+    //Gear ratio of the motor to the wheel. 1:1 would mean that 1 turn of the motor is one turn of the wheel, 2:1 would mean two turns of the motor is one turn of the wheel, and so on.
+    static final double DRIVE_GEAR_REDUCTION= 1; // This is < 1.0 if geared UP
+
+    //Diameter of the wheel in inches
+    static final double WHEEL_DIAMETER_IN = 3.93701; // For figuring circumference
+
+    //How many times the encoder counts a tick per inch moved. (Ticks per rev * Gear ration) / perimeter
+    static final double COUNTS_PER_IN = (COUNTS_PER_MOTOR_REV_Hex * DRIVE_GEAR_REDUCTION)/(WHEEL_DIAMETER_IN * 3.1415);
+
+
 
     HardwareMap hwMap = null;
 
@@ -35,6 +64,39 @@ public class Chassis {
         FRMotor = hwMap.get(DcMotor.class, "FRMotor");
         BRMotor = hwMap.get(DcMotor.class, "BRMotor");
 
+
+        //IMU
+        imu                             = hwMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit            = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit            = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled       = true;
+        parameters.useExternalCrystal   = true;
+        parameters.mode                 = BNO055IMU.SensorMode.IMU;
+        parameters.loggingTag           = "IMU";
+
+        //Since our Rev Expansion is in Vertical Position, so we need to Z & X
+
+        //Need to be in CONFIG mode to write to registers
+        imu.write8(BNO055IMU.Register.OPR_MODE,BNO055IMU.SensorMode.CONFIG.bVal & 0x0F);
+//        byte AXIS_MAP_CONFIG_BYTE = 0x6; //This is what to write to the AXIS_MAP_CONFIG register to swap y and z axes
+//        byte AXIS_MAP_SIGN_BYTE = 0x1; //This is what to write to the AXIS_MAP_SIGN register to negate the z axis
+        //Need to be in CONFIG mode to write to registers
+        imu.write8(BNO055IMU.Register.OPR_MODE,BNO055IMU.SensorMode.CONFIG.bVal & 0x0F);
+        TimeUnit.MILLISECONDS.sleep(100); //Changing modes requires a delay before doing anything else
+
+        //Write to the AXIS_MAP_CONFIG register
+//        imu.write8(BNO055IMU.Register.AXIS_MAP_CONFIG,AXIS_MAP_CONFIG_BYTE & 0x0F);
+
+        //Write to the AXIS_MAP_SIGN register
+//        imu.write8(BNO055IMU.Register.AXIS_MAP_SIGN,AXIS_MAP_SIGN_BYTE & 0x0F);
+
+        //Need to change back into the IMU mode to use the gyro
+        imu.write8(BNO055IMU.Register.OPR_MODE,BNO055IMU.SensorMode.IMU.bVal & 0x0F);
+
+        TimeUnit.MILLISECONDS.sleep(100); //Changing modes again requires a delay
+
+        imu.initialize(parameters);
 
 
         //Setting the direction
@@ -66,4 +128,230 @@ public class Chassis {
         BRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
     }
+
+    public void Drive(double speed, int distance) {
+
+        runtime.reset();
+        timeout_ms = 10000;
+
+        robotY += distance;
+        Location[1] = robotY;
+
+        int targetFL;
+        int targetFR;
+        int targetBR;
+        int targetBL;
+
+        int FLPos = FLMotor.getCurrentPosition();
+        int FRPos = FRMotor.getCurrentPosition();
+        int BLPos = BLMotor.getCurrentPosition();
+        int BRPos = BRMotor.getCurrentPosition();
+
+        targetFR = FRPos + (int) (distance * COUNTS_PER_IN);
+        targetBR = BRPos + (int) (distance * COUNTS_PER_IN);
+        targetFL = FLPos + (int) (distance * COUNTS_PER_IN);
+        targetBL = BLPos + (int) (distance * COUNTS_PER_IN);
+
+        //Set motor targets
+        FLMotor.setTargetPosition(targetFL);
+        BLMotor.setTargetPosition(targetBL);
+        FRMotor.setTargetPosition(targetFR);
+        BRMotor.setTargetPosition(targetBR);
+
+        //set the mode to go to the target position
+        FLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //Set the power of the motor.
+        FLMotor.setPower(speed);
+        FRMotor.setPower(speed);
+        BLMotor.setPower(speed);
+        BRMotor.setPower(speed);
+
+        while ((runtime.milliseconds() < timeout_ms) && (FLMotor.isBusy() && FRMotor.isBusy())) {
+
+        }
+        this.stopDriveMotors();
+    }
+
+    public void Strafe(double speed, int distance) {
+
+        robotX += distance;
+
+        runtime.reset();
+        timeout_ms = 10000;
+
+        robotX += distance;
+        Location[0] = robotX;
+
+        int targetFL;
+        int targetFR;
+        int targetBR;
+        int targetBL;
+
+        int FLPos = FLMotor.getCurrentPosition();
+        int FRPos = FRMotor.getCurrentPosition();
+        int BLPos = BLMotor.getCurrentPosition();
+        int BRPos = BRMotor.getCurrentPosition();
+
+        targetFR = FRPos - (int) (distance * COUNTS_PER_IN);
+        targetBR = BRPos + (int) (distance * COUNTS_PER_IN);
+        targetFL = FLPos + (int) (distance * COUNTS_PER_IN);
+        targetBL = BLPos - (int) (distance * COUNTS_PER_IN);
+
+        //Set motor targets
+        FLMotor.setTargetPosition(targetFL);
+        BLMotor.setTargetPosition(targetBL);
+        FRMotor.setTargetPosition(targetFR);
+        BRMotor.setTargetPosition(targetBR);
+
+        //set the mode to go to the target position
+        FLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //Set the power of the motor.
+        FLMotor.setPower(speed);
+        FRMotor.setPower(speed);
+        BLMotor.setPower(speed);
+        BRMotor.setPower(speed);
+
+        while ((runtime.milliseconds() < timeout_ms) && (FLMotor.isBusy() && FRMotor.isBusy())) {
+
+        }
+        this.stopDriveMotors();
+    }
+
+    public void stopDriveMotors(){
+        // Stop all motion;
+        FLMotor.setPower(0);
+        FRMotor.setPower(0);
+        BLMotor.setPower(0);
+        BRMotor.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        // FLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // FRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // BLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // BRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void DriveToPosition(double Speed, int posX, int posY, boolean forwardFirst) {
+        if(forwardFirst) {
+            this.Drive(Speed, -posY);
+            this.Strafe(Speed, -posX);
+        }
+        else{
+            this.Strafe(Speed, -posX);
+            this.Drive(Speed, -posY);
+        };
+        System.out.println(Arrays.toString(Location));
+    }
+    public float modAngle(float angle) {
+        angle = angle % 360;
+        if (angle < 0) {
+            angle += 360;
+        }
+        return angle;
+    }
+    public void turnRobotToAngle(float endAngle) {
+        org.firstinspires.ftc.robotcore.external.navigation.Orientation angle;
+        angle = imu.getAngularOrientation();
+
+        float angleStart = modAngle(angle.firstAngle);
+        float angleEnd = modAngle(endAngle);
+        float angleCurrent = angleStart;
+        float direction = 0;
+
+        if (modAngle((angleEnd - angleCurrent)) >= 180) {
+            //Go Clockwise
+            direction = -1;
+        } else if (modAngle((angleEnd - angleCurrent)) <= 180) {
+            //Go Counter Clockwise
+            direction = 1;
+        }
+
+        double pwr = -0.6;
+
+
+        while (Math.abs(angleCurrent - angleEnd) > 2) {
+            FLMotor.setPower(-pwr * direction);
+            FRMotor.setPower(pwr * direction);
+            BLMotor.setPower(-pwr * direction);
+            BRMotor.setPower(pwr * direction);
+            angle = imu.getAngularOrientation();
+            angleCurrent = modAngle(angle.firstAngle);
+
+        }
+        stopDriveMotors();
+
+
+    }
+    public void autoTurn(float turnAngle){
+        float desc_start = 10;
+        double acc = 1;
+        float turnDirection = java.lang.Math.signum(turnAngle);
+        float turnOffset = 10;
+        float startAngle;
+        float currentAngle;
+        float alreadyTurned = 0;
+        double turnSpeed = 0.3;
+
+        turnAngle = Math.abs(turnAngle) - turnOffset;
+
+
+        this.FLMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.BLMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.FRMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.BRMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        startAngle = imu.getAngularOrientation().firstAngle;
+
+        while (alreadyTurned< turnAngle){
+
+            FLMotor.setPower(- turnSpeed * turnDirection * acc);
+            FRMotor.setPower(turnSpeed * turnDirection* acc);
+            BLMotor.setPower(-turnSpeed * turnDirection * acc);
+            BRMotor.setPower(turnSpeed * turnDirection * acc);
+            currentAngle = imu.getAngularOrientation().firstAngle;
+            alreadyTurned = Math.abs(currentAngle - startAngle);
+
+            if (alreadyTurned>180){
+                alreadyTurned = 360 - alreadyTurned;
+            }
+        }
+        stopDriveMotors();
+
+    }
+
+    public void moveRobotAprilTags(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower    =  x -y -yaw;
+        double rightFrontPower   =  x +y +yaw;
+        double leftBackPower     =  x +y -yaw;
+        double rightBackPower    =  x -y +yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+        FLMotor.setPower(leftFrontPower);
+        FRMotor.setPower(rightFrontPower);
+        BLMotor.setPower(leftBackPower);
+        BRMotor.setPower(rightBackPower);
+    }
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AutoCommon.java
@@ -0,0 +1,329 @@
+/* Copyright (c) 2023 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import org.firstinspires.ftc.teamcode.Helper.Robot;
+
+
+import java.util.List;
+
+/*
+ * This OpMode illustrates the basics of using both AprilTag recognition and TensorFlow
+ * Object Detection.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
+ */
+@Autonomous(name = "AutoCommon", group = "Auto")
+public class AutoCommon extends LinearOpMode {
+    /**
+     * Common variables for all Auto.
+     */
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
+    // The variable to store our instance of the AprilTag processor.
+    private AprilTagProcessor aprilTag;
+
+    //Variables for AprilTag delivery
+
+    double DESIRED_DISTANCE = 12; //  this is how close the camera should get to the target (inches)
+
+    //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+    //  applied to the drive motors to correct the error.
+    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+    final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    final double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+    final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+    final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_STRAFE= 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_TURN  = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+
+
+    // The variable to store our instance of the TensorFlow Object Detection processor.
+    private TfodProcessor tfod;
+
+    //The variable to store our instance of the vision portal.
+    private VisionPortal myVisionPortal;
+
+    // Custom model with blue and red team elements.
+    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/FixedLightingCenterstage.tflite";
+    // Define the labels recognized in the model for TFOD (must be in training order!)
+    private static final String[] LABELS = {
+            "Blue",
+            "Red"
+    };
+
+    // X-coordinate of team element at the start of auto.
+    float teamElementX = 320; // Frame size is 640, default in the middle.
+    int targetSpikeMark = 2;
+    int targetAprilTag = 0; // This needs to be assigned to 1, 2, or 3, based on the detected value.
+
+    float recognition_size;
+
+
+    //Robot Object
+    public Robot robot = new Robot();
+
+    /**
+     * Variables to change for different autos.
+     * The logic in all four OpModes is identical, we only change these variables.
+     */
+    // Target value of April tag for yellow pixel.
+
+    /**
+     * All methods for the AutoOpMode, we keep them public so that other opmodes can use them.
+     */
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize
+        initDoubleVision();
+        myVisionPortal.setProcessorEnabled(aprilTag, true);
+        myVisionPortal.setProcessorEnabled(tfod, true);
+        robot.init(hardwareMap);
+//
+        detectTeamElement();
+        if(opModeInInit()) {
+            telemetryAprilTag();
+            telemetryTfod();
+            telemetry.update();
+        }
+
+        waitForStart();
+        boolean doneAuto = false;
+        while (opModeIsActive() && !doneAuto)  {
+
+            telemetryAprilTag();
+            telemetryTfod();
+            detectTeamElement();
+
+
+            /**
+             * Step 2: Deliver purple pixel to the detected Position.
+             * (common to all auto)
+             */
+
+            switch(targetSpikeMark){
+                case 1:
+                    GoToBackboard();
+                    break;
+                case 2:
+                    GoToBackboard();
+                    break;
+                case 3:
+                    GoToBackboard();
+                    break;
+            }
+
+            doneAuto = true;
+
+        } // end while loop
+
+    }  //end opMode
+
+
+
+    /**
+     * Initialize AprilTag and TFOD.
+     */
+    public void initDoubleVision() {
+        // -----------------------------------------------------------------------------------------
+        // AprilTag Configuration
+        // -----------------------------------------------------------------------------------------
+
+        aprilTag = new AprilTagProcessor.Builder()
+                .build();
+
+        // -----------------------------------------------------------------------------------------
+        // TFOD Configuration
+        // -----------------------------------------------------------------------------------------
+
+        tfod = new TfodProcessor.Builder()
+
+                // With the following lines commented out, the default TfodProcessor Builder
+                // will load the default model for the season. To define a custom model to load,
+                // choose one of the following:
+                //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
+                //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+                //.setModelAssetName(TFOD_MODEL_ASSET)
+                .setModelFileName(TFOD_MODEL_FILE)
+
+                // The following default settings are available to un-comment and edit as needed to
+                // set parameters for custom models.
+                .setModelLabels(LABELS)
+                //.setIsModelTensorFlow2(true)
+                //.setIsModelQuantized(true)
+                //.setModelInputSize(300)
+                //.setModelAspectRatio(16.0 / 9.0)
+
+                .build();
+
+
+        // -----------------------------------------------------------------------------------------
+        // Camera Configuration
+        // -----------------------------------------------------------------------------------------
+
+        if (USE_WEBCAM) {
+            myVisionPortal = new VisionPortal.Builder()
+                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                    .addProcessors(tfod, aprilTag)
+                    .build();
+        } else {
+            myVisionPortal = new VisionPortal.Builder()
+                    .setCamera(BuiltinCameraDirection.BACK)
+                    .addProcessors(tfod, aprilTag)
+                    .build();
+        }
+    }   // end initDoubleVision()
+
+    /**
+     * Add telemetry about AprilTag detections.
+     */
+    public void telemetryAprilTag() {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+    }   // end method telemetryAprilTag()
+
+    /**
+     * Add telemetry about TensorFlow Object Detection (TFOD) recognitions.
+     */
+    public double telemetryTfod() {
+        double x = 0;
+        double y;
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+            telemetry.addData(""," ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+        }   // end for() loop
+
+        return x;
+
+    }   // end method telemetryTfod()
+
+    public void detectTeamElement(){ // detect position of team element.
+        /**
+         * Step 1: Detect object
+         * (common to all Auto)
+         */
+        // Enable TFOD to detect object and store the value.
+        myVisionPortal.setProcessorEnabled(tfod, true);
+
+
+        //Get a recognition
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        float teamElemThresholdMax = 200;
+        float teamElemThresholdMin = 25;
+        for (Recognition recognition : currentRecognitions) {
+            if (recognition.getWidth() > teamElemThresholdMax ||
+                    recognition.getWidth() < teamElemThresholdMin) {
+                continue;
+            }
+            teamElementX = (recognition.getLeft() + recognition.getRight()) / 2;
+            recognition_size = recognition.getWidth();
+
+            break;
+        }
+
+
+        if(teamElementX < 120) {
+            targetSpikeMark = 3; //Left
+        } else if(teamElementX > 440) {
+            targetSpikeMark = 1; //Right
+        } else {
+            targetSpikeMark = 2; //Center
+        }
+
+        telemetry.addData("Tele_X", teamElementX);
+        telemetry.addData("Recognition Size", recognition_size);
+        telemetry.addData("Target Spike Mark", targetSpikeMark);
+
+        // Add offset to account for blue or red side.
+        targetAprilTag = targetSpikeMark;
+
+        if (myVisionPortal.getProcessorEnabled(aprilTag)) {
+            telemetry.addLine("AprilTag enabled");
+            telemetry.addLine();
+            telemetryAprilTag();
+        }
+        else if(myVisionPortal.getProcessorEnabled(tfod)) {
+            telemetry.addLine("TFOD enabled");
+            telemetry.addLine();
+            telemetryTfod();
+        }
+        // Push telemetry to the Driver Station.
+
+    }
+
+    /**
+     * Methods for driving the robot.
+     */
+
+    public void GoToBackboard() throws InterruptedException {
+        robot.chassis.DriveToPosition(0.7, -24,48,true);
+        robot.chassis.autoTurn(-100);
+        robot.chassis.Strafe(0.7,36);
+    }
+}   // end class
